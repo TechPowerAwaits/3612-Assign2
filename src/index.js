1039327d8dbfc1258e13ec80f371a00a14b8dfae
Richard Johnston <techpowerawaits@outlook.com>
2024-11-10 17:02:35 -0700

    Add project details
    Also adds some custom classes for manipulating links.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /*
+   * Purpose: Handles all the functionality of the classes beginning with `Link-`.
+   *
+   * Details: The following classes are recognized:
+   * - Link-text-as-link -- Copies the text into the href.
+   * - Link-email -- prepends "mailto:" to the href.
+   *
+   * The behavior is undefined if the classes are not used on an anchor element.
+   */
+  function handleLinkClasses() {
+    document.querySelectorAll(".Link-text-as-link").forEach((link) => {
+      link.setAttribute("href", link.textContent);
+    });
+    document.querySelectorAll(".Link-email").forEach((link) => {
+      const href = link.getAttribute("href");
+
+      if (href) {
+        link.setAttribute("href", `mailto:${href}`);
+      }
+    });
+  }
+
+  /*
+   * Purpose: Makes the home page visible.
+   */
   function showHome() {
     const home = document.querySelector("#home");
     home.style.display = "flex";
     home.style.justifyContent = "space-between";
   }
 
+  handleLinkClasses();
   showHome();
 });
