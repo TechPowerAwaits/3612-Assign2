4b81335d536f71aa5bb9d5b92bae552111d1f3be
Richard Johnston <techpowerawaits@outlook.com>
2024-11-21 17:06:03 -0700

    Remove strange Text-* classes
    Just served to overcomplicate things.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   /*
-   * Purpose: Handles all the functionality of the classes beginning with `Link-`.
-   *
-   * Details: The following classes are recognized:
-   * - Link-text-as-link -- Copies the text into the href.
-   * - Link-email -- prepends "mailto:" to the href.
-   *
-   * The behavior is undefined if the classes are not used on an anchor element.
-   */
-  function handleLinkClasses() {
-    document.querySelectorAll(".Link-text-as-link").forEach((link) => {
-      link.setAttribute("href", link.textContent);
-    });
-    document.querySelectorAll(".Link-email").forEach((link) => {
-      const href = link.getAttribute("href");
-
-      if (href) {
-        link.setAttribute("href", `mailto:${href}`);
-      }
-    });
-  }
-
-  /*
    * Purpose: An object containing all functionality and data specific to this
    * website.
    */
@@ -297,6 +275,5 @@ document.addEventListener("DOMContentLoaded", () => {
     F1.data._racesSection.appendChild(racesTable);
   }
 
-  handleLinkClasses();
   F1.state.switchToHome();
 });
